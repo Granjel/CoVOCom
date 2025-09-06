046e337d55a456e67f7495ed5a7979c1e94fd187
Rodrigo Granjel <granjel@gmail.com>
2025-09-06 22:40:14 +0200

    script to load all packages

--- code/01-load-packages.R
+++ code/01-load-packages.R
@@ -1,3 +1,5 @@
+# libraries
+
 # load packages ----------------------------------------------------------
 
 # load or install pacman
@@ -11,6 +13,7 @@ pacman::p_load(
   tidyverse, # includes ggplot2, dplyr, tidyr, readr, etc.
   ggpubr, # publication-ready plots
   ggrepel, # non-overlapping text labels
+  ggdist, # visualizing distributions
   lme4, # linear mixed models
   lmerTest, # p-values for lmer
   glmmTMB, # generalized linear mixed models
@@ -22,5 +25,7 @@ pacman::p_load(
   vegan, # ordination, PERMANOVA
   permute, # control for permutation schemes
   lattice, # base plotting system for some model outputs
-  reshape # legacy reshape tools (check if still needed)
+  reshape, # legacy reshape tools (check if still needed)
+  MetBrewer, # colors from painting at the Metropolitan Museum of Art in NY (https://github.com/BlakeRMills/MetBrewer)
+  MoMAColors # colors from painting at the Museum of Modern Art in NY (https://github.com/BlakeRMills/MoMAColors)
 )
