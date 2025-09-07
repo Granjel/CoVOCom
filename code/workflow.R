da7625bedcdce5f6a6454be9bebce1eb1afd2c4b
Rodrigo Granjel <granjel@gmail.com>
2025-09-08 00:31:23 +0200

    added a script to call all the scripts in the workflow in order

---
+++ code/workflow.R
@@ -0,0 +1,7 @@
+# workflow
+
+# run all analysis scripts in order
+source("code/04-emitter-damage.R")
+source("code/05-receiver-damage.R")
+source("code/06-voc-total.R")
+source("code/07-vocs-permanova.R")
