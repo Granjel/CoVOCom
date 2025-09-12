4fb763e77ceaa2afcd9761908f40420775b8d3f5
Rodrigo Granjel <granjel@gmail.com>
2025-09-12 19:12:14 +0200

    workflow up and working so far!

--- code/workflow.R
+++ code/workflow.R
@@ -3,5 +3,7 @@
 # run all analysis scripts in order
 source("code/04-emitter-damage.R")
 source("code/05-receiver-damage.R")
-source("code/06-voc-total.R")
-source("code/07-vocs-permanova-pcoa.R")
+source("code/06-vocs-total.R")
+source("code/07-vocs-permanova.R")
+source("code/08-vocs-pcoa-population.R")
+source("code/09-vocs-pcoa-treatment.R")
