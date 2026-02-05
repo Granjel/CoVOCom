35aa71bc01a9fec9e91b9a5db718fda5ca758dec
Rodrigo Granjel <granjel@gmail.com>
2026-02-05 12:53:58 +0100

    Alphabetize VOC types/compounds, rename soil plot, adjust legend position, and save vocs experiment plot

--- code/16-soil-samples.R
+++ code/16-soil-samples.R
@@ -28,14 +28,12 @@ soils_sum <- soils %>%
   ) %>%
   arrange(type, id)
 
-# plot: points for mean and error bars for sd; all of the same VOC type grouped together; abundance on x axis
-soil_blanks <- soils_sum %>%
-  group_by(type) %>%
-  mutate(max_mean = max(mean_abundance)) %>%
-  ungroup() %>%
-  arrange(desc(max_mean), type, desc(mean_abundance)) %>%
+# plot: points for mean and error bars for sd; order types alphabetically and compounds alphabetically within each type; abundance on x axis
+plot_soil_blanks <- soils_sum %>%
+  # order types alphabetically and compounds alphabetically within each type (reversed)
+  arrange(type, compound) %>%
   mutate(
-    type = factor(type, levels = unique(type)),
+    type = factor(type, levels = sort(unique(type))),
     compound = factor(compound, levels = rev(unique(compound)))
   ) %>%
   ggplot(aes(x = mean_abundance, y = compound, color = type)) +
@@ -62,12 +60,12 @@ soil_blanks <- soils_sum %>%
   # add a viridis color scale
   scale_color_viridis_d(begin = 0.075, end = 0.95) +
   # add legend inside plot area
-  theme(legend.position = c(0.725, 0.225))
+  theme(legend.position = c(0.725, 0.5))
 
 # save the plot
 ggsave(
   "figures/soil-samples-voc-abundance.jpeg",
-  soil_blanks,
+  plot_soil_blanks,
   width = 6,
   height = 7
 )
@@ -139,66 +137,8 @@ vocs_sum <- vocs_type %>%
   # remove nonsense values (NA/NaN)
   filter(!is.na(Mean))
 
-# # plot the same way as soils_sum but faceted by population and with different shape dodged points for treatment; color by type of VOC
-# vocs_sum %>%
-#   group_by(type) %>%
-#   mutate(max_mean = max(Mean)) %>%
-#   ungroup() %>%
-#   arrange(desc(max_mean), type, desc(Mean)) %>%
-#   mutate(
-#     type = factor(type, levels = unique(type)),
-#     compound = factor(compound, levels = rev(unique(compound)))
-#   ) %>%
-#   # reverse the factor levels for treatment so dodged points plot in reverse order
-#   mutate(treatment = forcats::fct_rev(factor(treatment))) %>%
-#   ggplot(aes(x = Mean, y = compound, color = type, shape = treatment)) +
-#   geom_point(
-#     position = position_dodge(width = 0.7),
-#     size = 2.75
-#   ) +
-#   geom_errorbarh(
-#     aes(
-#       xmin = Q05,
-#       xmax = Q95
-#     ),
-#     height = 0.3,
-#     size = 0.6,
-#     position = position_dodge(width = 0.7)
-#   ) +
-#   labs(
-#     # title = "Mean abundance of VOCs by population and treatment",
-#     x = expression(
-#       "Abundance (" * ng ~ h^{
-#         -1
-#       } *
-#         ")"
-#     ),
-#     y = "VOC compound",
-#     color = "VOC type",
-#     shape = "Treatment"
-#   ) +
-#   # add a viridis color scale
-#   scale_color_viridis_d(begin = 0.075, end = 0.95) +
-#   # adjust x axis padding
-#   scale_x_continuous(expand = expansion(mult = c(0.02, 0.02))) +
-#   # reverse the order of treatment items only in the legend
-#   guides(shape = guide_legend(reverse = TRUE)) +
-#   # remove rectangle around facet labels and make labels bold
-#   theme(
-#     strip.background = element_blank(),
-#     strip.text = element_text(face = "bold", size = 12)
-#   ) +
-#   facet_wrap(~population, ncol = 1)
-
-# # save the plot
-# ggsave(
-#   "figures/voc-abundance-by-population-and-treatment.jpeg",
-#   width = 8,
-#   height = 10
-# )
-
 # plot the same way as soils_sum but faceted by population and with different shape dodged points for treatment; color by type of VOC
-vocs_sum %>%
+plot_vocs_experiment <- vocs_sum %>%
   # order types alphabetically and compounds alphabetically within each type
   arrange(type, compound) %>%
   mutate(
@@ -249,6 +189,7 @@ vocs_sum %>%
 # save the plot
 ggsave(
   "figures/voc-abundance-by-population-and-treatment-transformed.jpeg",
+  plot_vocs_experiment,
   width = 8,
   height = 10
 )
