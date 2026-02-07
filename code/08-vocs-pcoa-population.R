f1675e4d0b99903fb727c6d72f62b3defbb3aafe
Rodrigo Granjel <granjel@gmail.com>
2026-02-07 20:17:38 +0100

    Refactor population VOC PCoA to load PERMANOVA RDS objects, split envfit centroids/arrows, and drop treatment PCoA section

--- code/08-vocs-pcoa-population.R
+++ code/08-vocs-pcoa-population.R
@@ -1,75 +1,90 @@
 # VOCs PCoA for populations
 
-# review and see that it matches the PCoA for treatments
-
 # load data and variables ------------------------------------------------
 
 # load packages and data
 source("code/03-load-data.R")
 
-# load the subset of data used for population-level PERMANOVA (created in code/07-vocs-permanova.R)
-vocs_subset <- read.csv("tables/permanova/data-subsets/vocs-subset-all.csv") # currently, the subset is "All" but it could be changed to a specific type of VOCs if desired
+# load saved subsets and distance matrices from PERMANOVA
+vocs_objects <- readRDS(
+  file.path(
+    "tables/permanova/objects-for-pcoa",
+    "vocs-subsets-and-distances.rds"
+  )
+)
+
+# choose which VOC type to analyze (must match safe_name keys)
+voc_type_key <- "all"
+
+# subset and distance matrix for this VOC type
+vocs_subset <- vocs_objects$subsets[[voc_type_key]]
+compounds_bray <- as.dist(vocs_objects$distances[[voc_type_key]])
 
 # PCoA -------------------------------------------------------------------
 
-# use capscale to do PCoA
+# partial PCoA: control for treatment, focus on population
 vocs_pcoa_population <- capscale(
-  compounds_bray ~ 1 + Condition(vocs_subset$treatment),
+  compounds_bray ~ 1 + Condition(treatment),
   data = vocs_subset,
-  comm = vocs_subset %>% dplyr::select(voc1:voc17)
+  comm = vocs_subset %>% dplyr::select(starts_with("voc"))
 )
 
-# extract eigenvalues
-eigenvalues_population <- eigenvals(vocs_pcoa_population)
+# extract eigenvalues from unconstrained axes
+eigenvalues_population <- vocs_pcoa_population$CA$eig
 
 # proportion of variance explained by each axis
 variance_explained_population <-
   eigenvalues_population / sum(eigenvalues_population)
 
 # site ordination scores for the first two axes
+site_scores <- scores(
+  vocs_pcoa_population,
+  scaling = 3,
+  correlation = TRUE
+)$sites
+
+# create a data frame with ordination scores and metadata for plotting
 ordination_scores_population <-
   data.frame(
-    id = rownames(vocs_pcoa_population$CA$u),
+    id = rownames(site_scores),
     population = vocs_subset$population,
     plant = vocs_subset$code,
-    MDS1 = scores(vocs_pcoa_population, scaling = 3, correlation = TRUE)$sites[,
-      1
-    ],
-    MDS2 = scores(vocs_pcoa_population, scaling = 3, correlation = TRUE)$sites[,
-      2
-    ]
+    MDS1 = site_scores[, 1],
+    MDS2 = site_scores[, 2]
   )
 
-# envfit for fitted order arrows
+# envfit for population centroids
 envfit_population <- envfit(
   vocs_pcoa_population,
-  vocs_subset %>% dplyr::select(code, population, voc1:voc17)
+  data.frame(population = vocs_subset$population)
 )
 
-# extract centroids for both populations
+# extract centroids for populations
 centroids_population <- data.frame(
-  x = gsub("population", "", rownames(envfit_population$factors$centroids)),
+  x = rownames(envfit_population$factors$centroids),
   MDS1 = envfit_population$factors$centroids[, 1],
   MDS2 = envfit_population$factors$centroids[, 2]
-) %>%
-  filter(x == "Bon" | x == "Cai")
+)
 
-# extract arrows for VOC compounds
+# envfit for VOC arrows
+envfit_vocs <- envfit(
+  vocs_pcoa_population,
+  vocs_subset %>% dplyr::select(starts_with("voc"))
+)
+
+# extract arrows for significant VOC compounds
 arrows_population <- data.frame(
-  x = rownames(envfit_population$vectors$arrows),
-  MDS1 = envfit_population$vectors$arrows[, 1],
-  MDS2 = envfit_population$vectors$arrows[, 2],
-  pval = envfit_population$vectors$pvals,
-  rsq = envfit_population$vectors$r
+  x = rownames(envfit_vocs$vectors$arrows),
+  MDS1 = envfit_vocs$vectors$arrows[, 1],
+  MDS2 = envfit_vocs$vectors$arrows[, 2],
+  pval = envfit_vocs$vectors$pvals,
+  rsq = envfit_vocs$vectors$r
 ) %>%
-  # keep only keep significant arrows with r2 >= 0.6
-  filter(pval <= 0.05, rsq >= 0.6)
+  dplyr::filter(pval <= 0.05, rsq >= 0.6)
 
-# change the names of the compounds to more meaningful names from vocs_info
+# change the names of the compounds
 arrows_population$x <-
   vocs_info$compound[match(arrows_population$x, vocs_info$id)]
-
-# can roman letters be used in labels? for example: α-Pinene
 
 # define axis labels with variance explained
 xlabel <- paste0(
@@ -192,7 +207,7 @@ p_pcoa_population <-
     axis.line = element_blank(),
     axis.text = element_blank(),
     axis.ticks = element_blank(),
-    legend.position = c(0.725, 0.9),
+    legend.position = c(0.15, 0.9),
     text = element_text(size = 12),
     plot.margin = margin(5, 5, 5, 5)
   ) +
@@ -205,217 +220,6 @@ f <- 0.9 # scaling factor for dimensions
 ggsave(
   "figures/pcoa-vocs-population-all.png",
   p_pcoa_population,
-  width = fig_width * f,
-  height = fig_height * f,
-  dpi = fig_dpi
-)
-
-
-# TREATMENT
-
-# VOCs PCoA for treatments
-
-# load data and variables ------------------------------------------------
-
-# run permanova script, which loads data and variables
-source("code/07-vocs-permanova.R")
-
-# PCoA -------------------------------------------------------------------
-
-# use capscale to do PCoA
-vocs_pcoa_treatment <- capscale(
-  dplyr::select(vocs, starts_with("voc")) ~ 1 + Condition(treatment),
-  data = vocs,
-  distance = "bray"
-)
-
-# extract eigenvalues
-eigenvalues_treatment <- eigenvals(vocs_pcoa_treatment)
-
-# proportion of variance explained by each axis
-variance_explained_treatment <-
-  eigenvalues_treatment / sum(eigenvalues_treatment)
-
-# site ordination scores for the first two axes
-ordination_scores_treatment <-
-  data.frame(
-    id = rownames(vocs_pcoa_treatment$CA$u),
-    treatment = vocs$treatment,
-    plant = vocs$code,
-    MDS1 = scores(vocs_pcoa_treatment, scaling = 3, correlation = TRUE)$sites[,
-      1
-    ],
-    MDS2 = scores(vocs_pcoa_treatment, scaling = 3, correlation = TRUE)$sites[,
-      2
-    ]
-  )
-
-# envfit for fitted order arrows
-envfit_treatment <- envfit(
-  vocs_pcoa_treatment,
-  vocs %>% dplyr::select(code, treatment, voc1:voc17)
-)
-
-# extract centroids for both treatments
-centroids_treatment <- data.frame(
-  x = gsub("treatment", "", rownames(envfit_treatment$factors$centroids)),
-  MDS1 = envfit_treatment$factors$centroids[, 1],
-  MDS2 = envfit_treatment$factors$centroids[, 2]
-) %>%
-  filter(x == "Control" | x == "Herbivore-induced")
-
-# extract arrows for VOC compounds
-arrows_treatment <- data.frame(
-  x = rownames(envfit_treatment$vectors$arrows),
-  MDS1 = envfit_treatment$vectors$arrows[, 1],
-  MDS2 = envfit_treatment$vectors$arrows[, 2],
-  pval = envfit_treatment$vectors$pvals,
-  rsq = envfit_treatment$vectors$r
-) %>%
-  # keep only keep significant arrows with r2 >= 0.6
-  filter(pval <= 0.05, rsq >= 0.6)
-
-# change the names of the compounds to more meaningful names from vocs_info
-arrows_treatment$x <-
-  vocs_info$compound[match(arrows_treatment$x, vocs_info$id)]
-
-# can roman letters be used in labels? for example: α-Pinene
-
-# define axis labels with variance explained
-xlabel <- paste0(
-  "MDS1 (",
-  round(variance_explained_treatment[1] * 100, 1),
-  "%)"
-)
-ylabel <- paste0(
-  "MDS2 (",
-  round(variance_explained_treatment[2] * 100, 1),
-  "%)"
-)
-
-# plot -------------------------------------------------------------------
-
-# global plot parameters
-transparency_pcoa <- 0.9
-
-# plot PCoA for treatments and VOCs
-p_pcoa_treatment <-
-  ordination_scores_treatment %>%
-  ggplot(aes(x = MDS1, y = MDS2)) +
-
-  # add dashed lines at 0,0
-  geom_hline(
-    aes(yintercept = 0),
-    lty = "dashed",
-    size = 0.5,
-    color = "grey30"
-  ) +
-  geom_vline(
-    aes(xintercept = 0),
-    lty = "dashed",
-    size = 0.5,
-    color = "grey30"
-  ) +
-
-  # add ellipses for treatments
-  stat_ellipse(
-    aes(color = treatment, fill = treatment),
-    geom = "polygon",
-    level = 0.95,
-    alpha = 0.1,
-    linewidth = 0.75
-  ) +
-
-  # add points for plants
-  geom_point(
-    aes(fill = treatment),
-    shape = 21,
-    size = 2,
-    stroke = 0.33,
-    color = "black",
-    alpha = transparency_pcoa
-  ) +
-
-  # add arrows for significant VOC compounds
-  geom_segment(
-    data = arrows_treatment,
-    aes(
-      x = 0,
-      xend = MDS1 * as.numeric(rsq),
-      y = 0,
-      yend = MDS2 * as.numeric(rsq)
-    ),
-    arrow = arrow(length = unit(0.25, "cm")),
-    colour = "black",
-    size = 0.7
-  ) +
-
-  # add centroids with colors for treatments and different shape than other points
-  geom_point(
-    data = centroids_treatment,
-    aes(x = MDS1, y = MDS2, color = x, fill = x),
-    shape = 24,
-    size = 3,
-    stroke = 0.55,
-    color = "black",
-    alpha = transparency_pcoa,
-    show.legend = FALSE
-  ) +
-
-  # add labels with the names of the VOCs well separated from each other and with a white background
-  geom_label_repel(
-    data = arrows_treatment,
-    aes(
-      x = MDS1 * as.numeric(rsq),
-      y = MDS2 * as.numeric(rsq),
-      label = x
-    ),
-    size = 3.25,
-    alpha = 0.5,
-    color = NA,
-    label.size = 0,
-    segment.color = NA,
-    max.overlaps = Inf
-  ) +
-
-  # trick to add transparent background but not transparent text!
-  geom_label_repel(
-    data = arrows_treatment,
-    aes(
-      x = MDS1 * as.numeric(rsq),
-      y = MDS2 * as.numeric(rsq),
-      label = x
-    ),
-    size = 3.25,
-    color = "black",
-    fill = NA,
-    segment.color = NA,
-    max.overlaps = Inf
-  ) +
-
-  # color palette for treatments
-  scale_fill_manual(name = "Treatment", values = pal_treat) +
-  scale_color_manual(name = "Treatment", values = pal_treat) +
-
-  # remove axiss lines
-  theme(
-    axis.line = element_blank(),
-    axis.text = element_blank(),
-    axis.ticks = element_blank(),
-    legend.position = c(0.8, 0.95),
-    # legend.position = "top",
-    text = element_text(size = 12),
-    plot.margin = margin(20, 5, 5, 5)
-  ) +
-
-  # add axis labels
-  labs(x = xlabel, y = ylabel)
-
-# save plot
-f <- 0.9 # scaling factor for dimensions
-ggsave(
-  "figures/pcoa-vocs-treatment-trans.png",
-  p_pcoa_treatment,
   width = fig_width * f,
   height = fig_height * f,
   dpi = fig_dpi
