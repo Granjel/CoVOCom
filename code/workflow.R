b6e4d3a9756d46e9addde9b947d03e8ae3d92229
Rodrigo Granjel <granjel@gmail.com>
2026-02-12 13:19:04 +0100

    Consolidate VOC analysis scripts and update workflow numbering

--- code/workflow.R
+++ code/workflow.R
@@ -7,17 +7,13 @@ source("code/06-vocs-total.R")
 source("code/07-vocs-permanova.R")
 source("code/08-vocs-pcoa-population.R")
 source("code/09-vocs-pcoa-treatment.R")
-# fix when Lucía confirms the final VOC classification with Greg
-# source("code/10-alcohols-esters.R")
-# source("code/11-glvs.R")
-# source("code/12-ketones.R")
-# source("code/13-long-chain-alkanes.R")
-# source("code/14-terpenes.R")
-source("code/15-vocs-traits-correlations.R")
-source("code/16-soil-samples.R")
+source("code/10-vocs-10-vocs-by-type.R")
+source("code/11-vocs-traits-correlations.R")
+source("code/12-soil-samples.R")
 
-# generate all figures for the paper; uncomment to run
+# generate all figures and tables for the paper; uncomment to run
 # source("code/code-paper-figures/fig1.R")
 # source("code/code-paper-figures/fig2.R")
 # source("code/code-paper-figures/fig3.R")
 # source("code/code-paper-figures/fig4.R")
+# source("code/code-paper-figures/table1.R")
